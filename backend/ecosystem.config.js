e11d0567c96df2ff9627a8f85c171171fc7519e5
ITuzov <ITuzov@korusconsulting.ru>
2024-06-08 14:14:40 +0300

    fix key

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -10,7 +10,7 @@ const {
 module.exports = {
   apps: [{
     name: 'api-service',
-    script: './dist/app.js',
+    script: './backend/dist/app.js',
     env: {
       NODE_ENV: 'production'
     },
@@ -19,7 +19,6 @@ module.exports = {
     }
   }],
 
-  // Настройка деплоя
   deploy: {
     production: {
       user: DEPLOY_USER,
@@ -28,7 +27,7 @@ module.exports = {
       repo: 'https://github.com/ituzov/nodejs-pm2-deploy.git',
       path: DEPLOY_PATH,
       'pre-deploy': `echo "Starting pre-deploy" && ls -la /c/Users/ituzov/.ssh && scp -i /c/Users/ituzov/.ssh/id_rsa -o StrictHostKeyChecking=no .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/.env`,
-      'post-deploy': `echo "Starting post-deploy" && cd ${DEPLOY_PATH}/current && npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production`,
+      'post-deploy': `echo "Starting post-deploy" && cd ${DEPLOY_PATH}/current/backend && npm install && npm run build && pm2 startOrRestart ${DEPLOY_PATH}/current/backend/ecosystem.config.js --env production && pm2 save`,
     },
   },
 };
