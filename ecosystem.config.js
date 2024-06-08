997cfcd65c4e68e404d0ee6c94c567f5391a9d0f
ITuzov <ITuzov@korusconsulting.ru>
2024-06-08 12:39:43 +0300

    fix key

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,7 +25,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/ituzov/nodejs-pm2-deploy',
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy': `scp .env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'cd backend && npm i && npm run build',
     },
   },
