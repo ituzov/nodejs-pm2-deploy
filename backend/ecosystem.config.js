a5406935678e686c49ac4fbd73a499900a80f65c
ITuzov <ITuzov@korusconsulting.ru>
2024-06-08 14:28:46 +0300

    fix key

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -26,8 +26,6 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/ituzov/nodejs-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'pre-setup': `ssh -i /c/Users/ituzov/.ssh/id_rsa -o StrictHostKeyChecking=no ${DEPLOY_USER}@${DEPLOY_HOST} 'rm -rf ${DEPLOY_PATH}/current'`,
-      'post-setup': `echo "Setup completed"`,
       'pre-deploy': `echo "Starting pre-deploy" && ls -la /c/Users/ituzov/.ssh && scp -i /c/Users/ituzov/.ssh/id_rsa -o StrictHostKeyChecking=no .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/.env`,
       'post-deploy': `echo "Starting post-deploy" && cd ${DEPLOY_PATH}/current/backend && npm install && npm run build && pm2 save`,
     },
