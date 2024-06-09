ac9532f470cf118c80a049fe4e7472a8db225baf
ITuzov <ITuzov@korusconsulting.ru>
2024-06-09 12:07:08 +0300

    crash

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,6 +18,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.get('/crash-test', (req, res) => {
+  res.send('Server will crash now...');
+  // Принудительный крах сервера
+  process.exit(1);
+});
 app.use(routes);
 app.use(errors());
 app.use(errorHandler);
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -14,6 +14,7 @@ const router = Router();
 router.post('/signup', validateUserBody, createUser);
 router.post('/signin', validateAuthentication, login);
 
+
 // все роуты, кроме /signin и /signup, защищены авторизацией;
 router.use(auth);
 router.use('/users', userRouter);
